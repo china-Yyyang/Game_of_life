4257f69218b517817072b794c504f509dd3ea0e3
KEVIN <43997638+china-Yyyang@users.noreply.github.com>
2020-09-07 16:19:59 +0800

    Add files via upload

---
+++ gamewidget.cpp
@@ -0,0 +1,250 @@
+#include <QMessageBox>
+#include <QTimer>
+#include <QMouseEvent>
+#include <QDebug>
+#include <QRectF>
+#include <QPainter>
+#include <qmath.h>
+#include "gamewidget.h"
+
+GameWidget::GameWidget(QWidget *parent) :
+    QWidget(parent),
+    timer(new QTimer(this)),
+//    generations(-1),
+    universeSize(50)
+{
+    timer->setInterval(300);
+//    m_masterColor = "#000";
+    m_masterColor.setNamedColor("white");
+    universe = new bool[(universeSize + 2) * (universeSize + 2)];
+    next = new bool[(universeSize + 2) * (universeSize + 2)];
+    connect(timer, SIGNAL(timeout()), this, SLOT(newGeneration()));
+    memset(universe, false, sizeof(bool)*(universeSize + 2) * (universeSize + 2));
+    memset(next, false, sizeof(bool)*(universeSize + 2) * (universeSize + 2));
+}
+
+GameWidget::~GameWidget()
+{
+    delete [] universe;
+    delete [] next;
+}
+
+//void GameWidget::startGame(const int &number)
+void GameWidget::startGame()
+{
+//    generations = number;
+    timer->start();
+}
+
+void GameWidget::stopGame()
+{
+    timer->stop();
+}
+
+void GameWidget::clear()
+{
+    for(int k = 1; k <= universeSize; k++) {
+        for(int j = 1; j <= universeSize; j++) {
+            universe[k*universeSize + j] = false;
+        }
+    }
+    gameEnds(true);
+    update();
+}
+
+void GameWidget::nextStep()
+{
+    timer->stop();//
+    newGeneration();
+}
+
+int GameWidget::cellNumber()
+{
+    return universeSize;
+}
+
+void GameWidget::setCellNumber(const int &s)
+{
+    universeSize = s;
+    resetUniverse();
+    update();
+}
+
+void GameWidget::resetUniverse()
+{
+    delete [] universe;
+    delete [] next;
+    universe = new bool[(universeSize + 2) * (universeSize + 2)];
+    next = new bool[(universeSize + 2) * (universeSize + 2)];
+    memset(universe, false, sizeof(bool)*(universeSize + 2) * (universeSize + 2));
+    memset(next, false, sizeof(bool)*(universeSize + 2) * (universeSize + 2));
+}
+
+QString GameWidget::dump()
+{
+    char temp;
+    QString master = "";
+    for(int k = 1; k <= universeSize; k++) {
+        for(int j = 1; j <= universeSize; j++) {
+            if(universe[k*universeSize + j] == true) {
+                temp = '*';
+            } else {
+                temp = 'o';
+            }
+            master.append(temp);
+        }
+        master.append("\n");
+    }
+    return master;
+}
+
+void GameWidget::setDump(const QString &data)
+{
+    int current = 0;
+    for(int k = 1; k <= universeSize; k++) {
+        for(int j = 1; j <= universeSize; j++) {
+            universe[k*universeSize + j] = data[current] == '*';
+            current++;
+        }
+        current++;
+    }
+    update();
+}
+
+int GameWidget::interval()
+{
+    return timer->interval();
+}
+
+void GameWidget::setInterval(int msec)
+{
+    timer->setInterval(msec);
+}
+
+bool GameWidget::isAlive(int k, int j)
+{
+    int power = 0;
+    power += universe[(k+1)*universeSize + j];
+    power += universe[(k-1)*universeSize + j];
+    power += universe[k*universeSize + (j+1)];
+    power += universe[k*universeSize + (j-1)];
+    power += universe[(k+1)*universeSize + (j-1)];
+    power += universe[(k-1)*universeSize + (j+1)];
+    power += universe[(k-1)*universeSize + (j-1)];
+    power += universe[(k+1)*universeSize + (j+1)];
+    if (((universe[k*universeSize + j] == true) && (power == 2)) || (power == 3))
+           return true;
+    return false;
+}
+
+void GameWidget::newGeneration()
+{
+//    if(generations < 0)
+//        generations++;
+    int notChanged=0;
+    for(int k=1; k <= universeSize; k++) {
+        for(int j=1; j <= universeSize; j++) {
+            next[k*universeSize + j] = isAlive(k, j);
+            if(next[k*universeSize + j] == universe[k*universeSize + j])
+                notChanged++;
+        }
+    }
+    if(notChanged == universeSize*universeSize) {
+        QMessageBox::information(this,
+                                 tr("Game lost sense"),
+                                 tr("The End. Now game finished because all the next generations will be the same."),
+                                 QMessageBox::Ok);
+        stopGame();
+        gameEnds(true);
+        return;
+    }
+    for(int k=1; k <= universeSize; k++) {
+        for(int j=1; j <= universeSize; j++) {
+            universe[k*universeSize + j] = next[k*universeSize + j];
+        }
+    }
+    update();
+//    generations--; //有什么意义吗？？
+//    if(generations == 0) {
+//        stopGame();
+//        gameEnds(true);
+//        QMessageBox::information(this,
+//                                 tr("Game finished."),
+//                                 tr("Iterations finished."),
+//                                 QMessageBox::Ok,
+//                                 QMessageBox::Cancel);
+//    }
+}
+
+void GameWidget::paintEvent(QPaintEvent *)
+{
+    QPainter p(this);
+    paintGrid(p);
+    paintUniverse(p);
+}
+
+void GameWidget::mousePressEvent(QMouseEvent *e)
+{
+    emit environmentChanged(true);
+    double cellWidth = (double)width()/universeSize;
+    double cellHeight = (double)height()/universeSize;
+    int k = floor(e->y()/cellHeight)+1;
+    int j = floor(e->x()/cellWidth)+1;
+    universe[k*universeSize + j] = !universe[k*universeSize + j];
+    update();
+}
+
+void GameWidget::mouseMoveEvent(QMouseEvent *e)
+{
+    double cellWidth = (double)width()/universeSize;
+    double cellHeight = (double)height()/universeSize;
+    int k = floor(e->y()/cellHeight)+1;
+    int j = floor(e->x()/cellWidth)+1;
+    int currentLocation = k*universeSize + j;
+    if(!universe[currentLocation]){                //if current cell is empty,fill in it
+        universe [currentLocation]= !universe[currentLocation];
+        update();
+    }
+}
+
+void GameWidget::paintGrid(QPainter &p)
+{
+    QRect borders(0, 0, width()-1, height()-1); // borders of the universe
+    QColor gridColor = m_masterColor; // color of the grid
+    gridColor.setAlpha(10); // must be lighter than main color
+    p.setPen(gridColor);
+    double cellWidth = (double)width()/universeSize; // width of the widget / number of cells at one row
+    for(double k = cellWidth; k <= width(); k += cellWidth)
+        p.drawLine(k, 0, k, height());
+    double cellHeight = (double)height()/universeSize; // height of the widget / number of cells at one row
+    for(double k = cellHeight; k <= height(); k += cellHeight)
+        p.drawLine(0, k, width(), k);
+    p.drawRect(borders);
+}
+
+void GameWidget::paintUniverse(QPainter &p)
+{
+    double cellWidth = (double)width()/universeSize;
+    double cellHeight = (double)height()/universeSize;
+    for(int k=1; k <= universeSize; k++) {
+        for(int j=1; j <= universeSize; j++) {
+            if(universe[k*universeSize + j] == true) { // if there is any sense to paint it
+                qreal left = (qreal)(cellWidth*j-cellWidth); // margin from left
+                qreal top  = (qreal)(cellHeight*k-cellHeight); // margin from top
+                QRectF r(left, top, (qreal)cellWidth, (qreal)cellHeight);
+                p.fillRect(r, QBrush(m_masterColor)); // fill cell with brush of main color
+            }
+        }
+    }
+}
+
+QColor GameWidget::masterColor()
+{
+    return m_masterColor;
+}
+
+void GameWidget::setMasterColor(const QColor &color)
+{
+    m_masterColor = color;
+    update();
+}
---
+++ gamewidget.h
@@ -0,0 +1,60 @@
+#ifndef GAMEWIDGET_H
+#define GAMEWIDGET_H
+
+#include <QColor>
+#include <QWidget>
+
+class GameWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit GameWidget(QWidget *parent = 0);
+    ~GameWidget();
+
+protected:
+    void paintEvent(QPaintEvent *);
+    void mousePressEvent(QMouseEvent *e);
+    void mouseMoveEvent(QMouseEvent *e);
+
+signals:
+    //when one of the cell has been changed,emit this signal to lock the universeSize
+    void environmentChanged(bool ok);
+    //when game is over or clear is called,emit it to unlock the universeSize
+    void gameEnds(bool ok);
+
+public slots:
+//    void startGame(const int &number = -1); // start
+    void startGame();
+    void stopGame(); // finish
+    void clear(); // clear
+    void nextStep(); //next
+
+    int cellNumber(); // number of the cells in one row
+    void setCellNumber(const int &s); // set number of the cells in one row
+
+    int interval(); // interval between generations
+    void setInterval(int msec); // set interval between generations
+
+    QColor masterColor(); // color of the cells
+    void setMasterColor(const QColor &color); // set color of the cells
+
+    QString dump(); // dump of current universe
+    void setDump(const QString &data); // set current universe from it's dump
+
+private slots:
+    void paintGrid(QPainter &p);
+    void paintUniverse(QPainter &p);
+    void newGeneration();
+
+private:
+    QColor m_masterColor;
+    QTimer* timer;
+    int generations;
+    bool* universe; // map
+    bool* next; // map
+    int universeSize;
+    bool isAlive(int k, int j); // return true if universe[k][j] accept rules
+    void resetUniverse();// reset the size of universe
+};
+
+#endif // GAMEWIDGET_H
